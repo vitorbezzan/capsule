f665a5cc284a37ba5028da2a06580a166dd0069e
Vitor Bezzan <vitor@bezzan.com>
2025-07-22 10:40:22 -0300

    Update version number, add base classes for capsule package, and refine Ruff configuration

--- src/capsule/__init__.py
+++ src/capsule/__init__.py
@@ -1,3 +1,3 @@
 """API entrypoint for the package."""
 
-__version__ = "1.0.0"
+__version__ = "202507.0001"
---
+++ src/capsule/base.py
@@ -0,0 +1,81 @@
+"""Base classes for the capsule package."""
+
+import typing as tp
+
+import numpy as np
+import pandas as pd
+from numpy.typing import NDArray
+from sklearn.base import BaseEstimator, ClassifierMixin, RegressorMixin
+from sklearn.utils.validation import assert_all_finite
+
+Input: tp.TypeAlias = NDArray[np.float64] | pd.DataFrame
+Output: tp.TypeAlias = NDArray[np.float64]
+
+
+class _ImplementsPredict(tp.Protocol):
+    """Protocol for classes that implement a predict method."""
+
+    def predict(self, X: Input) -> Output:
+        """Predict the output for the given input."""
+
+
+class _ImplementsProba(tp.Protocol):
+    """Protocol for classes that implements predict and predict_proba methods."""
+
+    def predict(self, X: Input) -> Output:
+        """Predict the output for the given input."""
+
+    def predict_proba(self, X: Input) -> Output:
+        """Predict the probabilities for the given input."""
+
+
+class BaseCapsule(BaseEstimator):
+    """Base class for all capsules."""
+
+    def __init__(
+        self,
+        model: _ImplementsPredict | _ImplementsProba,
+        X_test: Input,
+        y_test: Output,
+    ) -> None:
+        """Initialize the base capsule with a model and test data."""
+        super().__init__()
+
+        self.model = model
+
+        if X_test.shape[0] != y_test.shape[0]:
+            raise ValueError("X_test and y_test must have the same number of samples.")
+
+        assert_all_finite(X_test, allow_nan=True)
+
+    def fit(
+        self, X: Input, y: Output | None = None, **fit_params: dict
+    ) -> "BaseCapsule":
+        """Capsules cannot be fit."""
+        raise NotImplementedError("Capsules cannot be fit.")
+
+    def predict(self, X: Input) -> Output:
+        """Predict the capsule's output."""
+        return self.predict(X)
+
+
+class RegressionCapsule(BaseCapsule, RegressorMixin):
+    """Capsule for regression tasks."""
+
+    def __init__(
+        self, model: _ImplementsPredict, X_test: Input, y_test: Output
+    ) -> None:
+        """Initialize the regression capsule."""
+        super().__init__(model, X_test, y_test)
+
+
+class ClassificationCapsule(BaseCapsule, ClassifierMixin):
+    """Capsule for classification tasks."""
+
+    def __init__(self, model: _ImplementsProba, X_test: Input, y_test: Output) -> None:
+        """Initialize the classification capsule."""
+        super().__init__(model, X_test, y_test)
+
+    def predict_proba(self, X: Input) -> Output:
+        """Predict the probabilities for the given input."""
+        return self.predict_proba(X)
