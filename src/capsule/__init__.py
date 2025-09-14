9507d80d7bde0b4e36d7d88431b77a7fdc4e7085
Vitor Bezzan <vitor@bezzan.com>
2025-09-13 22:23:05 -0300

    Bump version to 202509.02 in __init__.py

--- src/capsule/__init__.py
+++ src/capsule/__init__.py
@@ -1,6 +1,6 @@
 """**API entrypoint for the package.**"""
 
-__version__ = "202509.01"
+__version__ = "202509.02"
 
 from .base import BaseCapsule as BaseCapsule
 from .classification import ClassificationCapsule as ClassificationCapsule
